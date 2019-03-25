364fc587755d14ad329c45c8c93f8922f0519f63
yuhao <1123345502@qq.com>
2019-03-25 21:45:55 +0800

    开发环境日志级别调整为debug

--- src/main/java/com/yh/bootdemo/dao/master/UserMapper.java
+++ src/main/java/com/yh/bootdemo/dao/master/UserMapper.java
@@ -4,7 +4,6 @@ import com.yh.bootdemo.model.User;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Mapper
